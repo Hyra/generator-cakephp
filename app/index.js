55d3e6b7b4c0526da7d082819f911f8621be739e
Hyra <stef@mindthecode.com>
2013-08-13 13:26:40 +0200

    Reworked installer procedure and cleanup

--- app/index.js
+++ app/index.js
@@ -8,8 +8,14 @@ var fs = require('fs-extra');
 var Np_cakephpGenerator = module.exports = function Np_cakephpGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
+  var done = this.async();
+
   this.on('end', function () {
     this.installDependencies({ skipInstall: options['skip-install'] });
+
+    passthru("app/Vendor/bin/cake -app app", function() {
+      done();
+    });
 
   });
 
@@ -34,45 +40,42 @@ Np_cakephpGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
+  var prompts = [
+    {
+      name: 'db_local_db',
+      message: 'Local database DB Name',
+      type: 'string',
+      default: 'cake_dev'
+    },
+    {
+      name: 'db_local_user',
+      message: 'Local database Username',
+      default: ''
+    },
+    {
+      name: 'db_local_pass',
+      message: 'Local database Password',
+      type: 'password'
+    }
+  ];
 
-  this.security_salt = _randomString(40);
-  this.security_seed = _randomString(30, '0123456789');
+  this.prompt(prompts, function (props) {
+    this.db_local_user = props.db_local_user;
+    this.db_local_pass = props.db_local_pass;
+    this.db_local_db = props.db_local_db;
+    cb();
+  }.bind(this));
 
-  cb();
-
-  // this.prompt(prompts, function (props) {
-  //   this.someOption = props.someOption;
-
-  //   cb();
-  // }.bind(this));
 };
 
 Np_cakephpGenerator.prototype.app = function app() {
-  // this.mkdir('app');
-
-  // Cake basis
-  // this.directory('_cakephp', './');
-
-  // this.copy('_cake', 'cake');
-
-  this.copy('composer.phar', 'composer.phar');
-
   this.copy('_composer.json', 'composer.json');
   this.copy('_package.json', 'package.json');
   this.copy('_bower.json', 'bower.json');
-};
-
-Np_cakephpGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('_gitignore', '.gitignore');
+  this.copy('_cake', 'cake');
   this.copy('editorconfig', '.editorconfig');
   this.copy('jshintrc', '.jshintrc');
-
-  // this.template('_cakephp/app/Config/core.php', 'app/Config/core.php');
 };
 
 Np_cakephpGenerator.prototype.composer = function composer() {
@@ -89,33 +92,76 @@ Np_cakephpGenerator.prototype.composer = function composer() {
 
 };
 
-// Np_cakephpGenerator.prototype.configuration = function configuration() {
-//   this.write('app/Config/database.php', [
-//     "<?php",
-//     "class DATABASE_CONFIG {",
-//     "",
-//     "    public $default = array(",
-//     "        'datasource' => 'Database/Mysql',",
-//     "        'persistent' => false,",
-//     "        'host' => 'localhost',",
-//     "        'login' => 'root',",
-//     "        'password' => 'root',",
-//     "        'database' => 'wpwp',",
-//     "        'prefix' => '',",
-//     "        'encoding' => 'utf8',",
-//     "        );",
-//     "",
-//     "    public $test = array(",
-//     "        'datasource' => 'Database/Mysql',",
-//     "        'persistent' => false,",
-//     "        'host' => 'localhost',",
-//     "        'login' => 'user',",
-//     "        'password' => 'password',",
-//     "        'database' => 'test_database_name',",
-//     "        'prefix' => '',",
-//     "        //'encoding' => 'utf8',",
-//     "        );",
-//     "}",
-//     "?>"
-//   ].join('\n'));
-// }
+Np_cakephpGenerator.prototype.cakesetup = function cakesetup() {
+  var cb = this.async();
+
+  fs.unlink('app/Controller/AppController.php');
+  fs.unlink('app/Model/AppModel.php');
+
+  this.copy('_cakefiles/_AppController.php', 'app/Controller/AppController.php');
+  this.copy('_cakefiles/_AppModel.php', 'app/Model/AppModel.php');
+
+  this.mkdir('app/Config/Environment');
+  this.copy('_cakefiles/Environment/_production.php', 'app/Config/Environment/production.php');
+  this.copy('_cakefiles/Environment/_staging.php', 'app/Config/Environment/staging.php');
+  this.template('_cakefiles/Environment/_local.php', 'app/Config/Environment/local.php');
+
+  this.copy('_cakefiles/_database.php', 'app/Config/database.php');
+
+  fs.readFile('app/webroot/index.php', 'utf8', function (err,data) {
+    if (err) return console.log(err);
+
+    var result = data.replace('//define(\'CAKE_CORE_INCLUDE_PATH\', ROOT . DS . \'lib\');', 'define(\'CAKE_CORE_INCLUDE_PATH\',  ROOT . DS . APP_DIR . \'/Vendor/cakephp/cakephp/lib\');');
+
+    fs.writeFile('app/webroot/index.php', result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
+
+  });
+
+  fs.readFile('app/Config/core.php', 'utf8', function (err,data) {
+    if (err) return console.log(err);
+
+    var result = data.replace('DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi', _randomString(40));
+    result = result.replace('76859309657453542496749683645', _randomString(30, '0123456789'));
+    result = result.replace("\t//date_default_timezone_set('UTC');", "date_default_timezone_set('Europe/Amsterdam');");
+
+    fs.writeFile('app/Config/core.php', result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
+
+  });
+
+  fs.appendFile('app/Config/bootstrap.php', [
+      "// Bootstrap for the different environments",
+      "if(isset($_SERVER['APPLICATION_ENV'])) {",
+      "  if($_SERVER['APPLICATION_ENV'] == 'development') {",
+      "    $_SERVER['APPLICATION_ENV'] = 'local';",
+      "    define('ENVIRONMENT', 'development');",
+      "  }",
+      "  Configure::load('Environment/'.$_SERVER['APPLICATION_ENV']);",
+      "} else {",
+      "  if(isset($_SERVER['SHELL'])) {",
+      "    Configure::load('Environment/local');",
+      "  } else {",
+      "    Configure::load('Environment/production');",
+      "  }",
+      "}",
+      "",
+      "// Load all Plugins",
+      "CakePlugin::loadAll();",
+      "",
+      "// Load composer autoload.",
+      "require APP . '/Vendor/autoload.php';",
+      "",
+      "// Remove and re-prepend CakePHP's autoloader as composer thinks it is the most important.",
+      "// See https://github.com/composer/composer/commit/c80cb76b9b5082ecc3e5b53b1050f76bb27b127b",
+      "spl_autoload_unregister(array('App', 'load'));",
+      "spl_autoload_register(array('App', 'load'), true, true);"
+    ].join('\n'), 'utf8', function (err) {
+      if (err) return console.log(err);
+  });
+
+  cb();
+
+};
